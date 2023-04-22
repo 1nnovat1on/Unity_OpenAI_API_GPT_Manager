532330daebfcecab7cd69dff7d1b93fd003bf658
Colin Jackson <43419182+1nnovat1on@users.noreply.github.com>
2023-04-22 17:54:45 -0400

    Create OpenAIAPIManager.cs

---
+++ OpenAIAPIManager.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class OpenAIAPIManager : MonoBehaviour
+{
+    public string model = "gpt-3.5-turbo";
+
+    [System.Serializable]
+    public class ChatCompletionResponse
+    {
+        public Choice[] choices;
+    }
+
+    [System.Serializable]
+    public class Choice
+    {
+        public Message message;
+    }
+
+    [System.Serializable]
+    public class Message
+    {
+        public string role;
+        public string content;
+    }
+
+    public string API_KEY;
+    private const string API_URL = "https://api.openai.com/v1/chat/completions";
+
+    public void GenerateCompletion(string prompt, Action<string> callback)
+    {
+        StartCoroutine(SendRequest(prompt, callback));
+    }
+
+private IEnumerator SendRequest(string prompt, Action<string> callback)
+{
+    // Update jsonBody to use the model variable
+    string jsonBody = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]}";
+
+    UnityWebRequest request = new UnityWebRequest(API_URL, "POST");
+    byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonBody);
+    request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+    request.downloadHandler = new DownloadHandlerBuffer();
+    request.SetRequestHeader("Content-Type", "application/json");
+    request.SetRequestHeader("Authorization", "Bearer " + API_KEY);
+
+    yield return request.SendWebRequest();
+
+    if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+    {
+        Debug.LogError("Error: " + request.error);
+        callback(null);
+    }
+    else
+    {
+        string response = request.downloadHandler.text;
+        string completion = ParseResponse(response);
+        callback(completion);
+    }
+}
+
+
+    private string ParseResponse(string response)
+    {
+        ChatCompletionResponse chatCompletionResponse = JsonUtility.FromJson<ChatCompletionResponse>(response);
+        if (chatCompletionResponse.choices.Length > 0)
+        {
+            return chatCompletionResponse.choices[0].message.content;
+        }
+        return "";
+    }
+}
